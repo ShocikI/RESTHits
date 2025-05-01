4001b2e9040f7492927665c0c45fda562a85ac06
ShocikI <szotmicha16@gmail.com>
2025-05-01 11:09:01 +0200

    Specify fields in serializers

--- hits/serializers.py
+++ hits/serializers.py
@@ -6,11 +6,16 @@ from hits.models import Artist, Hit
 class ArtistSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Artist
-        fields = '__all__'
+        fields = ['id', 'first_name', 'last_name', 'created_at']
 
 class HitSerializer(serializers.HyperlinkedModelSerializer):
+    artist_id = serializers.PrimaryKeyRelatedField(
+        queryset=Artist.objects.all()
+    )
+
     class Meta:
         model = Hit
-        fields = '__all__'
+        fields = ['title', 'artist_id', 'title', 'title_url', 'created_at', 'updated_at']
+        read_only_fields = ["title_url", "created_at", "updated_at"]
 
     
