08ddd45678ef016b0a5879a2e5edc0865904ff07
ShocikI <szotmicha16@gmail.com>
2025-05-02 17:04:13 +0200

    Improve serializers

--- hits/serializers.py
+++ hits/serializers.py
@@ -5,16 +5,21 @@ from hits.models import Artist, Hit
 
 class ArtistSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
+        """Meta options for ArtistSerializer."""
         model = Artist
-        fields = ['id', 'first_name', 'last_name', 'created_at']
+        fields = ['id', 'first_name', 'last_name', 'created_at', 'updated_at']
+        read_only_fields = ['id', 'created_at', 'updated_at']
 
-class HitSerializer(serializers.HyperlinkedModelSerializer):
+
+class HitSerializer(serializers.ModelSerializer):
+    """Serializer for the Hit model."""
     artist_id = serializers.PrimaryKeyRelatedField(
         queryset=Artist.objects.all(), 
         source="artist"
     )
-
+    
     class Meta:
+        """Meta options for HitSerializer."""
         model = Hit
         fields = ['title', 'artist_id', 'title', 'title_url', 'created_at', 'updated_at']
         read_only_fields = ["created_at", "updated_at"]
