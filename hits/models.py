848de104662c0f427f12e3a720dabd963ebb5249
ShocikI <szotmicha16@gmail.com>
2025-05-02 13:47:07 +0200

    Add __str__ method into models

--- hits/models.py
+++ hits/models.py
@@ -9,12 +9,19 @@ class Artist(models.Model):
     class Meta:
         ordering = ['created_at']
 
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
 class Hit(models.Model):
     title = models.CharField(max_length=128, blank=False)
     artist = models.ForeignKey(Artist, on_delete=models.CASCADE, blank=False)
-    title_url = models.CharField(max_length=256, blank=False)
+    title_url = models.CharField(max_length=256, blank=False, unique=True)
     created_at = models.DateField(auto_now_add=True)
     updated_at = models.DateField(auto_now=True)
 
     class Meta:
         ordering = ['-created_at']
+    
+    def __str__(self):
+        return f"{self.artist.first_name} {self.artist.last_name} - {self.title}"
